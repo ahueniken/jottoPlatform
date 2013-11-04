17b31130fddef7fb36bca40b9e30425a0fda11f6
Adam Hueniken <adam.hueniken@gmail.com>
2013-11-04 01:19:23 -0500

    signin & signout added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 JottoPlatform::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'pages#home'
   match '/about',   to: 'pages#about',   via: 'get'
   match '/signup',   to: 'users#new',   via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
