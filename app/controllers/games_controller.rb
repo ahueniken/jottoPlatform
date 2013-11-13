609ee4ff0a7c1094e0ac5fcbddfc329d03c8d4ba
Adam Hueniken <adam.hueniken@gmail.com>
2013-11-12 23:07:11 -0500

    able to add and track guesses

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
-  
+  @@setupCompleted = false
+
   def new
   	@game = Game.new
   end
@@ -7,10 +8,18 @@ class GamesController < ApplicationController
   def show
   	@game = Game.find(params[:id])
     if @game.word1 != nil && @game.word2 != nil
-      @setupflag = true
+      @@setupCompleted = true
     else
-      @setupflag = false
+      @@setupCompleted = false
     end
+
+    if (@game.p1guesses != nil)
+      @p1guessarray = @game.p1guesses.split(',')
+    end
+    if (@game.p2guesses != nil)
+      @p2guessarray = @game.p2guesses.split(',')
+    end
+
   end
 
   def create
@@ -24,23 +33,50 @@ class GamesController < ApplicationController
 
   def update
   	@game = Game.find(params[:id])
-    if current_user.id == @game.user1
-      if @game.update_attribute(:word1, params[:game][:word1]) 
-        flash[:success] = params[:game][:word1]
-        redirect_to @game
-      else
-        flash[:error] = "Errpr"
-        render 'show'
+   
+    if !@@setupCompleted
+      if current_user.id == @game.user1
+        if @game.update_attribute(:word1, params[:game][:word1]) 
+          flash[:success] = params[:game][:word1]
+          redirect_to @game
+        end
+      elsif current_user.id == @game.user2
+        if @game.update_attribute(:word2, params[:game][:word2])
+          flash[:success] = params[:game][:word2]
+          redirect_to @game
+        end
       end
-    elsif current_user.id == @game.user2
-      if @game.update_attribute(:word2, params[:game][:word2])
-        flash[:success] = params[:game][:word2]
-        redirect_to @game
-      else
-        flash[:error] = "Errpr"
-        render 'show'
+
+    else
+      if current_user.id == @game.user1
+        if (@game.p1guesses != nil)
+          guesses = @game.p1guesses + "," + params[:game][:p1guesses]
+        else 
+          guesses = params[:game][:p1guesses]
+        end
+        if @game.update_attribute(:p1guesses, guesses) 
+          flash[:success] = params[:game][:p1guesses]
+          redirect_to @game
+        end
+      elsif current_user.id == @game.user2
+        if (@game.p2guesses != nil)
+          guesses = @game.p2guesses + "," + params[:game][:p2guesses]
+        else 
+          guesses = params[:game][:p2guesses]
+        end
+        if @game.update_attribute(:p2guesses, guesses)
+          flash[:success] = params[:game][:p2guesses]
+          redirect_to @game
+        end
       end
     end
+
+
+  end
+
+  helper_method :setupCompleted
+  def setupCompleted
+    @@setupCompleted
   end
 
 
